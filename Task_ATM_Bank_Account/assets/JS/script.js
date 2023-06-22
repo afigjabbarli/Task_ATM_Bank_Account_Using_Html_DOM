5d979bb164e43dd6ce43ae4ab232fc07e12e0527
afigjabbarli <afiqcabbarli96@gmail.com>
2023-06-22 00:33:16 -0700

    Balance Menu succesfully created

--- Task_ATM_Bank_Account/assets/JS/script.js
+++ Task_ATM_Bank_Account/assets/JS/script.js
@@ -1,12 +1,71 @@
-// Add a click event handler to the Balance element
-const balanceElement = document.querySelector('.Balance');
+
+const amount = 20000;
+const balanceElement = document.querySelector('.Balance-display');
 balanceElement.addEventListener('click', function(event) {
-  // Prevent the default behavior of the click event
+  
   event.preventDefault();
-  // Add your custom logic here to handle the click event
-  // For example, you can hide all elements within the .container class
+ 
   const container = document.querySelector('.container');
   const tittle = document.querySelector('.tittle')
+  const displayBalance = document.querySelector('.Balance-none')
+  displayBalance.style.display = 'flex';
   tittle.style.display = 'none';
   container.style.display = 'none';
+  const currencyLists = document.querySelectorAll(".Balance-none ul li h1")
+  currencyLists.forEach((liElements) => {
+    let content = liElements.textContent;
+    if(content === "azn"){
+      content = currencyRates(amount, "manat");
+      
+      liElements.innerHTML = content
+    }
+    if(content === "lira"){
+      content = currencyRates(amount, "lira")
+      liElements.innerHTML = content
+    }
+    if(content === "rubl"){
+      content = currencyRates(amount, "rubl")
+      liElements.innerHTML = content
+    }
+    if(content === "dollar"){
+      content = currencyRates(amount, "dollar")
+      liElements.innerHTML = content
+    }
+    if(content === "sterling"){
+      content = currencyRates(amount, "sterling")
+      liElements.innerHTML = content
+    }
+    if(content === "euro"){
+      content = currencyRates(amount, "avro")
+      liElements.innerHTML = content
+    }
+  })
 });
+
+
+function currencyRates(amount, monetary){
+   if(monetary === "manat"){
+     return amount;
+   }
+   if(monetary === "lira"){
+     let balance = amount / 0.0874;
+     return balance;
+   }
+   if(monetary === "rubl"){
+    let balance = amount / 0.0209;
+    return balance;
+   }
+   if(monetary === "dollar"){
+    let balance = amount / 1.7;
+    return balance;
+   }
+   if(monetary === "sterling"){
+    let balance = amount / 2.1223;
+    return balance;
+   }
+   if(monetary === "avro"){
+    let balance = amount / 1.8726;
+    return balance
+   }
+}
+
